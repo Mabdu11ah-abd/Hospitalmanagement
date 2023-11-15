3c73f187e2798a31af2a7d5fb3082c73e3f6367f
Kanz-Ul-Eman <144201117+Kanz-Ul-Eman@users.noreply.github.com>
2023-11-15 19:09:08 +0500

    Add files via upload

--- OOP_project/Patient.java
+++ OOP_project/Patient.java
@@ -1,25 +1,71 @@
+import java.util.Objects;
+import java.util.Scanner;
+
 public class Patient {
+    private String name;
+    private String ID;
+    private int age;
+    private String address;
+    private String status;
+    private Doctor DoctorIncharge;
+    private Bed BedUsed;
+    private MedicalRecord Record;
+    private Billing Bill;
+    private Prescription patientprescription;
 
-private String name;
-private String ID;
+    public void SetPatient() {
+        // scanner variable for input
+        Scanner input = new Scanner(System.in);
 
-private String Status;
-private Doctor Doctorincharge;
-private Bed BedUsed;
-private MedicalRecord Record;
-private Billing Bill;
-private Prescription patientprescription;
+        // taking user input for item attributes
+        System.out.println("Enter the name of the patient : ");
+        this.name = input.nextLine();
+        System.out.println("Enter the ID of the patient : ");
+        input.nextLine();
+        this.ID = input.nextLine();
+        System.out.println("Enter the Address of the patient : ");
+        this.address = input.nextLine();
+        System.out.println("Enter the Doctor Incharge Name of the patient : ");
+        this.DoctorIncharge = input.next();
+        System.out.println("Enter the Status of the patient : ");
+        //Married, unmarried.
+        this.status = input.nextLine();
+        System.out.println("Enter the age of the patient : ");
+        //Married, unmarried.
+        this.age = input.nextInt();
+    }
+    public void DisplayPatient() {
 
-public String getID() {
-    return ID;
-}
+        // TODO: Write statements and format output for the method
 
-public void addPatient(){
-}
-public void UpdatePatient(){
-}
-public void DisplayPatient(){   
-}
-public void UpdateBilling(){
-}
+        System.out.println(name);
+        System.out.println(ID);
+        System.out.println(status);
+        System.out.println(DoctorIncharge);
+        System.out.println(age);
+        System.out.println(address);
+    }
+    public void updatePatient(String ID, String name, int age, String status, String address) {
+            if (this.ID.equals(ID)) {
+                this.name = name;
+                this.age = age;
+                this.status = status;
+                this.address = address;
+                System.out.println("Patient information updated for Patient ID: " + ID);
+            } else {
+                System.out.println("Patient ID not found.");
+            }
+    }
+
+    public void addPatient(String name, int age, String ID, String address, String status) {
+        this.name = name;
+        this.age = age;
+        this.ID = ID;
+        this.address = address;
+        this.status = status;
+        System.out.println("Patient information updated for Patient ID: " + ID);
+    }
+
+    public void UpdateBilling() {
+    }
 }
