7a56b4a900accbcb2fc41e36a15c61c396607dc7
Kanz-Ul-Eman <144201117+Kanz-Ul-Eman@users.noreply.github.com>
2023-11-15 15:19:46 +0500

    Add files via upload

--- OOP_project/User.java
+++ OOP_project/User.java
@@ -1,12 +1,52 @@
 public class User {
-    private int ID;
-    private String role;
-    private String Password;
 
-    public void RegisterUser() {
+    private String username;
+    private String password;
+    private String role;  // Admin, Doctor ,Patient
+
+    public User(String username, String password, String role) {
+        setUsername(username);
+        setPassword(password);
+        setRole(role);
     }
 
-    public void LoginUser(){ 
+    public String getUsername() {
+        return username;
     }
 
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    // Method to authenticate the user
+    public boolean login(String enteredUsername, String enteredPassword) {
+        if (enteredUsername.equals(username) && enteredPassword.equals(password)) {
+            System.out.println("Login successful. Welcome, " + username + "!");
+            return true;
+        } else {
+            System.out.println("Invalid username or password. Please try again.");
+            return false;
+        }
+    }
+
+    //Method to logout.
+    public void logout() {
+        System.out.println("Logging out. Goodbye, " + username + "!");
+    }
 }
