c60304b56ea007d6641b58f44ba1582dc12a1001
Kanz-Ul-Eman <144201117+Kanz-Ul-Eman@users.noreply.github.com>
2023-11-15 15:18:59 +0500

    Add files via upload

--- OOP_project/Bed.java
+++ OOP_project/Bed.java
@@ -1,10 +1,42 @@
 public class Bed {
-    private Boolean Status;
-    private float Price;
-    public void AllocateRoom()
-    {}
-    public void FreeRoom()
-    {
+    private int bedNumber;
+    private boolean isOccupied;
+
+    // Getters and Setters for all fields
+    public int getBedNumber() {
+        return bedNumber;
     }
 
+    public void setBedNumber(int bedNumber) {
+        this.bedNumber = bedNumber;
+    }
+
+    public boolean isOccupied() {
+        return isOccupied;
+    }
+
+    public void setOccupied(boolean occupied) {
+        isOccupied = occupied;
+    }
+    // Method to occupy the bed
+    public void occupyBed() {
+        if (!isOccupied) {
+            isOccupied = true;
+            System.out.println("Bed " + bedNumber + " is now occupied.");
+        } else {
+            System.out.println("Bed " + bedNumber + " is already occupied.");
+        }
+    }
+
+    // Method to vacate the bed
+    public void vacantBed() {
+        if (isOccupied) {
+            isOccupied = false;
+            System.out.println("Bed " + bedNumber + " is now vacant.");
+        } else {
+            System.out.println("Bed " + bedNumber + " is already vacant.");
+        }
+    }
+
+    // Other relevant methods
 }
