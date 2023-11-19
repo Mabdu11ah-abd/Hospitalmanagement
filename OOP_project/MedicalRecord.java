bbd9e099ecd1e847571256cbc33e0fee1e218736
Kanz-Ul-Eman <144201117+Kanz-Ul-Eman@users.noreply.github.com>
2023-11-19 07:57:44 +0500

    added a method to see all or latest prescription

--- OOP_project/MedicalRecord.java
+++ OOP_project/MedicalRecord.java
@@ -1,16 +1,69 @@
 import java.util.ArrayList;
-/*
- * Remove Record variable and age Variable 
- * Change Admission Date and DischargeDate to String 
- * Make it so that user can view all previous prescriptions or Find Prescription by date
- * Make it so that patient can view latest prescription
- * Do all of this in one method viewPrescriptions 
- * fix addPrescription method 
- */
+import java.util.Date;
+import java.util.Scanner;
 public class MedicalRecord {
-    private String admissionDate;
-    private String dischargeDate;
+    private int age;
+    private Date admissionDate;
+    private Date dischargeDate;
+    private String record;
     private ArrayList<Prescription> prescriptions; // As Prescription is another class
+
+    // Getters and Setters for all fields
+    public int getAge() {
+        return age;
+    }
+
+    public MedicalRecord() {
+    }
+
+    public MedicalRecord(int age, Date admissionDate, Date dischargeDate) {
+        setAge(age);
+        setAdmissionDate(admissionDate);
+        setDischargeDate(dischargeDate);
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Date getAdmissionDate() {
+        return admissionDate;
+    }
+
+    public void setAdmissionDate(Date admissionDate) {
+        this.admissionDate = admissionDate;
+    }
+
+    public Date getDischargeDate() {
+        return dischargeDate;
+    }
+
+    public void setDischargeDate(Date dischargeDate) {
+        this.dischargeDate = dischargeDate;
+    }
+
+    public String getRecord() {
+        return record;
+    }
+
+    public void setRecord(String record) {
+        this.record = record;
+    }
+
+    public void setPrescriptions(ArrayList<Prescription> prescriptions) {
+        this.prescriptions = prescriptions;
+    }
+
+    // Method to update the patient's record
+    public void updateRecord(String newRecord) {
+        if (admissionDate != null && dischargeDate == null) {
+            // Only allow updating record if the patient is currently admitted
+            this.record = newRecord;
+            System.out.println("Record updated: " + newRecord);
+        } else {
+            System.out.println("Cannot update record. The patient is either discharged or not admitted.");
+        }
+    }
 
     // Method to add a prescription to the medical record
     public void addPrescription(Prescription prescription) {
@@ -20,6 +73,24 @@ public class MedicalRecord {
 
         } else {
             System.out.println("Cannot add prescription. The patient is either discharged or not admitted.");
+        }
+    }
+    //Method to get data of all prescription of the patient.
+    ArrayList<Prescription> allPrescriptions = new ArrayList<>(prescriptions);
+    public void getPrescriptions() {
+        System.out.println("Enter 1 to see all prescriptions \n Enter 2 to see latest prescription");
+        Scanner input = new Scanner(System.in);
+        int choice = input.nextInt();
+        if (choice == 1) {
+            for (int i = 0; i < allPrescriptions.size(); i++) {
+                System.out.println(allPrescriptions.get(i));
+            }
+        } else if (choice == 2) {
+            int latestPrescription = allPrescriptions.size() - 1;
+            System.out.println(allPrescriptions.get(latestPrescription));
+        }
+        else {
+            System.out.println("Invalid Choice");
         }
     }
 }
